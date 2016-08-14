281c1180f8c22627990b4b3e248ebc91aec83969
s9p0b <asvatukhin@gmail.com>
2016-08-14 16:27:14 +0700

    Update hanoi.rkt

--- TowerOfHanoi/hanoi.rkt
+++ TowerOfHanoi/hanoi.rkt
@@ -2,217 +2,257 @@
 
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-advanced-reader.ss" "lang")((modname Hanoi3.2) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
+#reader(lib "htdp-advanced-reader.ss" "lang")((modname hanoi4.1) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
 (require racket/list)
+;; all changes are based on 3.0
+(define POLES 5)
+(define RINGS 4)
 
-;;=======CONSTANTS========
-
-(define POLES 3)
-(define RINGS 3) ; number of rings (1 is the smalles, 4 is the largest)
-(define ALL-VALS (build-list RINGS add1)) 
-
-
-
-;; Ring is Natural
-;; ring with its number as a size from 1
-(define RING1 1) ; smallest ring
-(define RING4 4) ; largest ring
-
-
-
-;; Pole is (listof Ring)
-;; a pole with rings
-(define POLE1 (list 1 2 3 4))
-;  1
-;  2
-;  3
-;  4 
-
-(define POLE2 (list 1 2 4))
-;  1
-;  2
-;  4
-
-(define POLE3 empty)           ;empty pole
-
-
-
-;; Game is (listof Pole)
-;; list of poles
-
-(define BLANK
-  (make-list POLES '()))
-; (list '()
-;       '()
-;       '())
-
-
-(define GAME1        ; invalid
-  (list '()          ; |  |  |
-        '(3 2)       ; |  3  1   
-        '(1 4)))     ; |  2  4
-
-(define GAME2        ;     
-  (list '(3 4)       ; |  |  |
-        '(1)         ; 3  |  |
-        '(2)))       ; 4  1  2
+(define ALL-VALS (build-list RINGS add1))
 
 (define STARTB
   (cons (build-list RINGS add1) (make-list (- POLES 1) '())))
 
-; (list '(1 2 3 4)
-;       '()
-;       '())
-
-;  1  |  |
-;  2  |  |
-;  3  |  |
-;  4  |  |
-
-(define WINB
-  (reverse STARTB))
-
-; (list '()
-;       '()
-;       '(1 2 3 4))
-
-; |  |  1  
-; |  |  2  
-; |  |  3 
-; |  |  4
-
-
-
-;;=======FUNCTIONS========
-
-
-;; Game -> Game
-;; produce a solution for g; or false if g is unsolvable
-;; Assume: g is valid
-
 (define (solve g)
-  (local [(define (solve--g g visited)
-            (if (solved? g)
-                (list (reverse visited) (length visited))
+  (local [(define (solve--gm g visited)
+            (if (solve? g)
+                (list (reverse visited) (- (length visited) 1))
                 (solve--log (next-games-w/filter g visited) visited)))
           
           (define (solve--log log visited)
-            (cond [(empty? log) false]
+            (solve--gm (first log) (cons (make--situation (first log)) visited)))]
+    
+    (solve--gm g (list (make--situation g)))))
+
+;; criterion to win !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! this changed !!!!! (3.2)
+(define (solve? g)
+  (ormap (λ(x) (= (length x) RINGS)) (rest g)))
+
+
+;; filter w/ previous situation
+(define (next-games-w/filter g pre)
+  (local [(define (newlst g pre)
+            (cond [(empty? g) empty]
                   [else
-                   (local [(define try (solve--g (first log) (cons (make--situation (first log)) visited)))]
-                     (if (not (false? try))
-                         try
-                         (solve--log (rest log) visited)))]))]
-    (solve--g g (list (make--situation g)))))
+                   ;;(if (ormap (λ(x) (fixed? x 2)) g)
+                   ;;  (takeit
+                   (if (member? (make--situation (first g)) pre)
+                       (newlst (rest g) pre)
+                       (cons (first g) (newlst (rest g) (cons (make--situation (first g)) pre))))]))]
+    (newlst (next-games g) pre)))
 
 
 
-;; Game -> Boolean
-;; produce true if game is solved
 
-; (check-expect (solved? WINB) true)
-; (check-expect (solved? GAME2) false)
+;; next possible moves
+(define (next-games g)
+  (local [(define (next--games vals)
+            (cond [(empty? vals) empty]
+                  [else
+                   (append (val-to-game g (first vals))
+                           (next--games (rest vals)))]))]
+    (next--games (can-move-vals g))))
+
+
+
+
+;; value to game board
+(define (val-to-game g val)
+  (local [(define (val-game movs)
+            (cond [(empty? movs) empty]
+                  [else
+                   (cons (putit (takeit g val) val (first movs))
+                         (val-game (rest movs)))]))]
+    (val-game (;find-poles-w/filter
+               find-poles-w/-fixed
+               g val))))
+
+#;#;#;
+;; !!!!!!!!!!!! find pole with filter !!!???? something wrong here
+(check-expect (find-poles-w/filter (list '()
+                                         '(3 4)
+                                         '(2)
+                                         '()
+                                         '(1))
+                                   1) (list 2 1))
+(check-expect (find-poles-w/filter (list '(3 4)
+                                         '(2)
+                                         
+                                         '(1))
+                                   1) (list 1 0))
+(define (find-poles-w/filter g r)
+  (local [(define (check-not-empty lop x)
+            (not (empty? (list-ref lop x))))
+          
+          (define (check-empty lop x)
+            (empty? (list-ref lop x)))
+          
+          (define try (find-poles g r))
+          
+          (define (1-length? lop r)
+            (cond [(empty? lop) false]
+                  [else
+                   (if (member? r (first lop))
+                       (= (length (first lop)) 1)
+                       (1-length? (rest lop) r))]))]
+    
+    (sort (append (filter (λ(x) (check-not-empty g x)) try)
+                  (if (1-length? g r)
+                      empty
+                      (local [(define try2 (filter (λ(x) (check-empty g x)) try))]
+                        (if (not (empty? try2))
+                            (list (first try2))
+                            empty))))
+          >)))
+
+
+(define (find-poles-w/-fixed g r)
+  (local [(define (find-poles g num)
+            (local [(define (find-pole g rsf acc g1)
+                      (cond [(empty? g) rsf] ; here
+                            [else
+                             (find-pole (rest g)
+                                        (if (and (changed? (putit g num 0) g1) (or (empty? (first g)) (< num (first (first g)))))
+                                            (cons acc rsf)
+                                            rsf)
+                                        (add1 acc)
+                                        (rest g1))]))]
+              (find-pole (takeit g num) empty 0 g)))]
+    (if (fixed? g (+ r 1))
+        (find-fixed g (+ r 1))
+        (find-poles g r))))
+
+(define (find-fixed g r)
+  (local [(define (find-the-fixed g acc)
+            (cond [(empty? g) empty]
+                  [else
+                   (if (member? r (first g))
+                       (list acc)
+                       (find-the-fixed (rest g) (add1 acc)))]))]
+    (find-the-fixed g 0)))
+
+
 
 #;
-(define (solved? g)
-  (if (= (length (list-ref g (- POLES 1))) RINGS)
-      (andmap = (list-ref g (- POLES 1)) ALL-VALS)
-      false))
-
-(define (solved? g)
-  (ormap (lambda(x) (= (length x) RINGS)) (rest g)))
-
-
-;; Game (listof Game) -> (listof Game)
-;; produce all the games possible after one steps, where each game has not been visited yet
-
-(define (next-games-w/filter g visited)
-  (local [(define (newlist log visited)
-            (cond [(empty? log) empty]
-                  [else
-                   (if (and (not (empty-pole-inbetween? (remove-initial-empty-poles (make--situation (first log)))))
-                            (not (member? (make--situation (first log)) visited)))
-                       
-                       (cons (first log) (newlist (rest log) (cons (make--situation (first log)) visited)))
-                       (newlist (rest log) visited))]))]
-    (newlist (next-games g) visited)))
-
-
-
-;; Game -> (listof Game)
-;; produce all the games possible after one steps
-
-; (check-expect (next-games STARTB) (list (list '(2 3 4)
-;                                               '(1)
-;                                               '())
-;                                         (list '(2 3 4)
-;                                               '()
-;                                               '(1))))
-; (check-expect (next-games GAME2) (list (list '(1 3 4)
-;                                              '()
-;                                              '(2))
-;                                        (list '(3 4)
-;                                              '()
-;                                              '(1 2))
-;                                        (list '(2 3 4)
-;                                              '(1)
-;                                              '())))
-
-
-
-(define (next-games g)
-  (local [(define (next--games lor)
-            (cond [(empty? lor) empty]
-                  [else
-                   (append (ring-to-game g (first lor))
-                           (next--games (rest lor)))]))]
-    (next--games (can-move-rings g))))
-
-
-
-;; Game Ring -> (listof Game)
-;; produce all games possible after moving the given ring
-
-; (check-expect (ring-to-game STARTB 1) (list (list '(2 3 4)
-;                                                   '(1)
-;                                                   '())
-;                                             (list '(2 3 4)
-;                                                   '()
-;                                                   '(1))))
-; (check-expect (ring-to-game GAME2 1) (list (list '(1 3 4)
-;                                                  '()
-;                                                  '(2))
-;                                            (list '(3 4)
-;                                                  '()
-;                                                  '(1 2))))
-
-
-(define (ring-to-game g r)
-  (local [(define (ring-game poles)
-            (cond [(empty? poles) empty]
-                  [else
-                   (cons (putit (takeit g r) r (first poles))
-                         (ring-game (rest poles)))]))]
-    (ring-game (find-poles g r))))
-
-
-
-;; Game Ring -> (listof Natural)
-;; find valid poles to move the given ring to
-
-; (check-expect (find-poles STARTB 1) (list 1 2))
-; (check-expect (find-poles GAME2 2) (list 0))
-; (check-expect (find-poles GAME2 1) (list 0 2))
-
-
-(define (find-poles g r)
-  (local [(define (find-pole g rsf acc gprev)
-            (cond [(empty? g) rsf]
+;; find valid pole to move for a value !!!!!!!!!!!!!!!!!!!!!! this changed  !!!! (3.2)
+(define (find-poles g num)
+  (local [(define (find-pole g rsf acc g1)
+            (cond [(empty? g) rsf] ; here
                   [else
                    (find-pole (rest g)
-                              (if (and (changed? (putit g r 0) gprev) (or (empty? (first g)) (< r (first (first g)))))
+                              (if (and (changed? (putit g num 0) g1) (or (empty? (first g)) (< num (first (first g)))))
                                   (cons acc rsf)
                                   rsf)
                               (add1 acc)
-                              (rest gprev))]))]
-    (find-pole (takeit g r) empty 0 g)))
+                              (rest g1))]))]
+    (find-pole (takeit g num) empty 0 g)))
+
+
+
+;; movable values !!!!!!!!!!!!!!!!!!!!!! this changed !!!!!!!! 
+(define (can-move-vals g)
+  ; here (3.2)
+  (reverse (filter (λ(x) (and (can-take? g x) (can-drop? g x)
+                              (not (fixed? g x)) ;; !!!! here(4.0)
+                              )) ALL-VALS)))
+
+
+;; take the move one
+(define (takeit g num)
+  (cond [(empty? g) empty]
+        [else
+         (cons (if (empty? (first g))
+                   (first g)
+                   (if (= (first (first g)) num)
+                       (rest (first g))
+                       (first g)))
+               (takeit (rest g) num))]))
+
+;; put the move one at a position
+(define (putit g num pos)
+  (cond [(empty? g) empty]
+        [else
+         (if (= pos 0)
+             (cons (cons num (first g)) (putit (rest g) num (sub1 pos)))
+             (cons (first g) (putit (rest g) num (sub1 pos))))]))
+
+;; ASSUME: Board is not BLANK
+(define (can-take? g num)
+  (cond [(empty? g) false]
+        [else
+         (if (not (empty? (first g)))
+             (if (= (first (first g)) num)
+                 true
+                 (can-take? (rest g) num))
+             (can-take? (rest g) num))]))
+
+;; ASSUME: Board not BLANK and Valid
+(define (can-drop? g num)
+  (cond [(empty? g) false]
+        [else
+         (if (not (empty? (first g)))
+             (if (> (first (first g)) num)
+                 true
+                 (can-drop? (rest g) num))
+             true)]))
+
+;; check if situation is unchanged if ring is put
+(define (changed? g pre)
+  (or (not (= (length (make--situation g)) (length (make--situation pre))))
+      (not (andmap = (make--situation g) (make--situation pre)))))
+
+;; A situation
+(define (make--situation g)
+  (cond [(empty? g) empty]
+        [else
+         (append (first g) '(0) (make--situation (rest g)))]))
+
+
+;; !!!!!!!!!!!!!!! NEW feature in 4.0 
+;; make the number fixed, e.g in four rings, if 4 is not on the first pole, then cannot move 4 again, if 3 is upon 4 and 4 is fixed, cannot move 3 again
+#;#;#;#;#;
+(check-expect (fixed? (list '(1 2 3)
+                            '(4)
+                            '())
+                      4) true)
+(check-expect (fixed? (list '(3 4)
+                            '(1 2)
+                            '())
+                      3) false)
+(check-expect (fixed? (list '(4)
+                            '(1 2 3)
+                            '())
+                      4) false)
+(check-expect (fixed? (list '(1 2)
+                            '(3 4)
+                            '())
+                      3) true)
+
+(check-expect (fixed? (list '(1)
+                            '(4)
+                            '(2 3))
+                      2)
+              false)
+(define (fixed? g r)
+  (cond [(= r RINGS) (if (member? r (first g))
+                         false
+                         true)]
+        [else
+         (local [(define (isfixed? lop r) 
+                   (cond [(empty? lop) false]
+                         [else
+                          (if (andmap (λ(x) (member? x (first lop))) (filter (λ(x) (> x (- r 1))) ALL-VALS))
+                              true
+                              (isfixed? (rest lop) r))]))]
+           (isfixed? (rest g) r))]))
+
+
+;; avoid the move from empty pole to empty pole
+
+
+
+
+
+
+
+(solve STARTB)
